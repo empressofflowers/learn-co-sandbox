9736d7d983b8d6079656e0767c2f0ac07ffa2220
Devone Austin <devone.austin@gmail.com>
2020-06-15 15:12:09 +0000

    Automatically backed up by Learn

---
+++ learning-comments.rb
@@ -0,0 +1,9 @@
+# Don't do this:
+puts ( 1 #+ 1)
+
+# Perform a constant epression evaluation
+3
+# Assign constant 3 to bare-word variable triangle_sides
+'triangle_sides'
+
+# From the Three Dog Night song: "Joy to the World (Jeremiach was a Bullfrong)"
